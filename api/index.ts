309a225623d13600e3faf9addc581cd2a2d55c1a
Katsuki6608 <katsuki6608@gmail.com>
2026-08-20 11:44:44 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,4 @@
-import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { hiAnimeRoutes } from '../src/routes/routes'
-
-const app = new Hono()
-
-app.get('/ping', (c) => c.json({ status: 'ok' }))
-app.route('/api', hiAnimeRoutes) // agar routes.ts ke andar already /v2/hianime/home defined hai
+import app from '../src/app'
 
 export default handle(app)
