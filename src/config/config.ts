578f44bc48def29089407dfac9e3c13860c84f8e
Katsuki6608 <katsuki6608@gmail.com>
2026-08-20 19:10:14 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,18 +1,20 @@
 const config = {
-  baseurl: 'https://aniwatchtv.to',
-  baseurl2: 'https://aniwatchtv.to',
+  baseurl: 'https://hianime.to',
+  baseurl2: 'https://hianime.to',
   origin: '*',
   port: 5000,
 
   headers: {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
   },
 
   logLevel: 'INFO',
   enableLogging: false,
   isProduction: true,
   isDevelopment: false,
-  isVercel: false,
+  isVercel: true,
 };
 
 export default config;
