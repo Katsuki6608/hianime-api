70c397eed79580b0b5ac100daed4c4ea354d170d
Katsuki6608 <katsuki6608@gmail.com>
2026-08-20 19:55:18 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,17 @@
 const config = {
-  baseurl: 'https://hianime.to',
+  baseurl: 'https://aniwatchtv.to',
   baseurl2: 'https://hianime.to',
   origin: '*',
   port: 5000,
 
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    Accept:
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'no-cache',
+    Pragma: 'no-cache',
   },
 
   logLevel: 'INFO',
